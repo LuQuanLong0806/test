7223f3913789ef58c07212c52bf3b0540bf43937
鹿全龙 <luql@yeyoo.cn>
2023-01-16 20:51:07 +0800

    没改啥

--- src/main.js
+++ src/main.js
@@ -8,6 +8,11 @@ import filters from '@/util/filters'
 import Alert from './components/modules/alert'
 import Pop from './components/modules/pop'
 
+
+import "i-ui123/dist/css/index.css";
+import iUi from "i-ui123";
+Vue.use(iUi);
+
 // 公共过滤器
 Object.keys(filters).forEach(d => {
     Vue.filter(d, filters[d])
