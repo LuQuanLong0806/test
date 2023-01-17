916532344c7f1e20abab9f184ac14faf5894d707
鹿全龙 <luql@yeyoo.cn>
2023-01-17 23:15:11 +0800

    修改

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -117,48 +117,48 @@ export default {
     ValidationProvider,
     ValidationObserver,
   },
-  directives: {
-    drag: {
-    inserted: function (el, binding, vnode) {
-        console.log("el", el, binding, vnode);
-        // 初始化
-        vnode.context.__imgX = 0;
-        vnode.context.__imgY = 0;
-        vnode.context.__imgXY = {
-            x: 0,
-            y: 0
-        };
-        let o = vnode.context.__imgXY;
-        let c = vnode.context;
-        el.onmousedown = (e) => {
-            // e.stopPropagation();
-            e.preventDefault();
-            o.x = e.x;
-            o.y = e.y;
-            el.onmousemove = (e) => {
-                let x2 = e.x;
-                let y2 = e.y;
-                // 获取移动距离
-                let x = x2 - o.x; // +-
-                let y = y2 - o.y;
+//   directives: {
+//     drag: {
+//     inserted: function (el, binding, vnode) {
+//         console.log("el", el, binding, vnode);
+//         // 初始化
+//         vnode.context.__imgX = 0;
+//         vnode.context.__imgY = 0;
+//         vnode.context.__imgXY = {
+//             x: 0,
+//             y: 0
+//         };
+//         let o = vnode.context.__imgXY;
+//         let c = vnode.context;
+//         el.onmousedown = (e) => {
+//             // e.stopPropagation();
+//             e.preventDefault();
+//             o.x = e.x;
+//             o.y = e.y;
+//             el.onmousemove = (e) => {
+//                 let x2 = e.x;
+//                 let y2 = e.y;
+//                 // 获取移动距离
+//                 let x = x2 - o.x; // +-
+//                 let y = y2 - o.y;
 
-                c.__imgX += x;
-                c.__imgY += y;
+//                 c.__imgX += x;
+//                 c.__imgY += y;
 
-                // 讲当前位置保存方便下次移动进行比较
-                o.x = x2;
-                o.y = y2;
+//                 // 讲当前位置保存方便下次移动进行比较
+//                 o.x = x2;
+//                 o.y = y2;
 
-                el.style.transform =
-                    'translate(' + c.__imgX + 'px' + ',' + c.__imgY + 'px' + ')';
-            };
-        };
-        el.onmouseup = () => {
-            el.onmousemove = null;
-        };
-    }
-    },
-  },
+//                 el.style.transform =
+//                     'translate(' + c.__imgX + 'px' + ',' + c.__imgY + 'px' + ')';
+//             };
+//         };
+//         el.onmouseup = () => {
+//             el.onmousemove = null;
+//         };
+//     }
+//     },
+//   },
   data() {
     return {
       svg: "",
